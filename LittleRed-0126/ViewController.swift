487c3d197de18d239c4d628ea90576853300bef8
XWill-cn <xuwei.cs.cn@gmail.com>
2021-01-26 22:07:43 +0800

    Initial Commit

---
+++ LittleRed-0126/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittleRed-0126
+//
+//  Created by 徐维 on 2021/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
